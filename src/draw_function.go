3df984a8e53480bd56a82ace6e546a90130188de
Maxim Kosterov <maxim.kosterov@gmail.com>
2022-12-03 16:44:32 +0300

    splitting draw.go

--- src/draw_function.go
+++ src/draw_function.go
@@ -3,85 +3,9 @@ package src
 import (
 	"regexp"
 	"strings"
-	"sync"
 
 	"github.com/gookit/color"
-	"github.com/rs/zerolog/log"
 )
-
-type DrawState struct {
-	Document CsvDocument
-}
-
-type UpdateMessage int8
-
-const (
-	StateUpdate UpdateMessage = iota
-	Exit
-)
-
-type DrawRoutineCommunication struct {
-	StateUpdate    chan UpdateMessage
-	TuiClosed      chan bool
-	DrawState      *DrawState
-	DrawStateMutex *sync.Mutex
-}
-
-func CreateCommunication() DrawRoutineCommunication {
-	state := DrawState{}
-	var mutex sync.Mutex
-
-	return DrawRoutineCommunication{
-		StateUpdate:    make(chan UpdateMessage),
-		TuiClosed:      make(chan bool),
-		DrawState:      &state,
-		DrawStateMutex: &mutex,
-	}
-}
-
-func (communication *DrawRoutineCommunication) UpdateState(updater func(*DrawState)) {
-	communication.DrawStateMutex.Lock()
-	updater(communication.DrawState)
-	communication.DrawStateMutex.Unlock()
-	communication.StateUpdate <- StateUpdate
-}
-
-func (communication *DrawRoutineCommunication) Close() {
-	communication.StateUpdate <- Exit
-	<-communication.TuiClosed
-}
-
-func RunDrawRoutine() DrawRoutineCommunication {
-	communication := CreateCommunication()
-
-	go func() {
-		canvas := CreateCanvas()
-		log.Info().Msg("Created canvas")
-
-		for message := range communication.StateUpdate {
-			log.Info().Interface("message", message).Msg("Processing message")
-
-			if message == Exit {
-				log.Info().Msg("Exiting drawer")
-				canvas.Batch(func(b *Batch) { b.Clear() })
-				communication.TuiClosed <- true
-				return
-			}
-
-			if message == StateUpdate {
-				communication.DrawStateMutex.Lock()
-				canvas.Batch(func(b *Batch) {
-					DrawFunction(b, communication.DrawState)
-				})
-				communication.DrawStateMutex.Unlock()
-			}
-		}
-	}()
-
-	communication.StateUpdate <- StateUpdate
-
-	return communication
-}
 
 // const (
 //   BOX_DRAWING_HORIZONTAL = "─"
